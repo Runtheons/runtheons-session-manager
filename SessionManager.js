42b32e3358584ecca3369b3aeafcd57972096485
Zexal0807 <gallinar00@gmail.com>
2021-01-11 15:05:48 +0100

    Fix - indentation

--- SessionManager.js
+++ SessionManager.js
@@ -3,27 +3,26 @@ const key = "RUNTHEONS";
 
 module.exports = new class SessionManager {
 
-    generateToken(data) {
-        var token = jwt.sign(
-            data,
-            key, {
-                expiresIn: "2 days"
-            }
-        );
-        return token;
-    }
+	generateToken(data) {
+		var token = jwt.sign(
+			data,
+			key, {
+				expiresIn: "2 days"
+			}
+		);
+		return token;
+	}
 
-    extractData(token) {
-        if (token == null)
-            return {};
+	extractData(token) {
+		if (token == null)
+			return {};
 
-        try {
-            var data = jwt.verify(token, key);
-            return data;
-        } catch (err) {
-            console.log(err);
-            return {};
-        }
-    }
-
+		try {
+			var data = jwt.verify(token, key);
+			return data;
+		} catch (err) {
+			console.log(err);
+			return {};
+		}
+	}
 }
