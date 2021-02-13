0f86a08d10b3f1574053837b6c2b8075c9d62155
Zexal0807 <gallinar00@gmail.com>
2021-02-13 16:38:38 +0100

    Version 1.1.1, add setOption

--- SessionManager.js
+++ SessionManager.js
@@ -3,12 +3,16 @@ const key = "RUNTHEONS";
 
 module.exports = new class SessionManager {
 
+	option = {
+		expiresIn: "2 days"
+	};
+
+	setOption(option) {
+		this.option = option;
+	}
+
 	generateToken(data) {
-		var token = jwt.sign({ data },
-			key, {
-				expiresIn: "2 days"
-			}
-		);
+		var token = jwt.sign({ data }, key, this.option);
 		return token;
 	}
 
