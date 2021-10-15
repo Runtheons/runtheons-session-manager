5ae7e01153529c08f42ed2d282eaadd95eea3287
Zexal0807 <gallinar00@gmail.com>
2021-10-15 16:46:26 +0200

    Implement crypto

--- SessionManager.js
+++ SessionManager.js
@@ -1,6 +1,10 @@
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 
+const ENCRYPTION_ALGORITHM = 'aes256';
+const ENCRYPTION_INPUT_ENCODING = 'utf-8';
+const ENCRYPTION_OUTPUT_ENCODING = 'hex';
+
 module.exports = class SessionManager {
 	static JWTkey = null;
 
@@ -23,18 +27,38 @@ module.exports = class SessionManager {
 	}
 
 	generateToken(data) {
-		if (SessionManager.JWTkey == null) {
-			throw new Error('Please set SessionManager JWTkey');
+		if (SessionManager.JWTkey == null || SessionManager.key == null) {
+			throw new Error('Please set SessionManager JWTkey and key');
 		}
 		var token = jwt.sign({ data }, SessionManager.JWTkey, this.option);
-		return token;
+		var cipher = crypto.createCipher(ENCRYPTION_ALGORITHM, SessionManager.key);
+		var encrypted = cipher.update(
+			token,
+			ENCRYPTION_INPUT_ENCODING,
+			ENCRYPTION_OUTPUT_ENCODING
+		);
+		encrypted += cipher.final(ENCRYPTION_OUTPUT_ENCODING);
+		return encrypted;
 	}
 
 	extractData(token) {
 		if (token == null) return {};
 
+		if (SessionManager.JWTkey == null || SessionManager.key == null) {
+			throw new Error('Please set SessionManager JWTkey and key');
+		}
 		try {
-			var data = jwt.verify(token, SessionManager.JWTkey);
+			var decipher = crypto.createDecipher(
+				ENCRYPTION_ALGORITHM,
+				SessionManager.key
+			);
+			var decrypted = decipher.update(
+				token,
+				ENCRYPTION_OUTPUT_ENCODING,
+				ENCRYPTION_INPUT_ENCODING
+			);
+			decrypted += decipher.final(ENCRYPTION_INPUT_ENCODING);
+			var data = jwt.verify(decrypted, SessionManager.JWTkey);
 			return data.data;
 		} catch (err) {
 			return undefined;
