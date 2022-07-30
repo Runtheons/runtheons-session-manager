06fb4de1fb65271fcee3e68d455e50612746849d
Zexal0807 <gallinar00@gmail.com>
2022-07-30 11:31:37 +0200

    Rename tests folder

--- tests/index.test.js
+++ tests/index.test.js
@@ -10,7 +10,7 @@ describe('Encryption', function() {
 	});
 
 	it('Without set JWTkey no data', async() => {
-		const SessionManager = require('./../index');
+		const SessionManager = require('../index');
 
 		expect(() => {
 			const result = SessionManager.generateToken(data);
@@ -18,36 +18,34 @@ describe('Encryption', function() {
 	});
 
 	it('Without set key no data', async() => {
-		const SessionManager = require('./../index');
-		require('./../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
+		const SessionManager = require('../index');
+		require('../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
 		expect(() => {
 			const result = SessionManager.generateToken(data);
 		}).toThrow();
 	});
 
 	it('With key length not equals to 32', async() => {
-		const SessionManager = require('./../index');
-		require('./../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
-		require('./../SessionManager').setKey(
-			'SECRET-RUNTHEONS-KEY-THAT-IS-NOT-32B'
-		);
+		const SessionManager = require('../index');
+		require('../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
+		require('../SessionManager').setKey('SECRET-RUNTHEONS-KEY-THAT-IS-NOT-32B');
 		expect(() => {
 			const result = SessionManager.generateToken(data);
 		}).toThrow();
 	});
 
 	it('Example', async() => {
-		require('./../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
-		require('./../SessionManager').setKey('SECRET-RUNTHEONS-KEY-THAT-IS-32B');
-		const SessionManager = require('./../index');
+		require('../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
+		require('../SessionManager').setKey('SECRET-RUNTHEONS-KEY-THAT-IS-32B');
+		const SessionManager = require('../index');
 		const result = SessionManager.generateToken(data);
 		assert.equal(typeof result, 'string');
 	});
 
 	it('Example with custom expiressIn', async() => {
-		require('./../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
-		require('./../SessionManager').setKey('SECRET-RUNTHEONS-KEY-THAT-IS-32B');
-		const SessionManager = require('./../index');
+		require('../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
+		require('../SessionManager').setKey('SECRET-RUNTHEONS-KEY-THAT-IS-32B');
+		const SessionManager = require('../index');
 		SessionManager.setOption({ expiresIn: '10 days' });
 		const result = SessionManager.generateToken(data);
 		assert.equal(typeof result, 'string');
@@ -60,7 +58,7 @@ describe('Decryption', function() {
 	});
 
 	it('Without set JWTkey no data', async() => {
-		const SessionManager = require('./../index');
+		const SessionManager = require('../index');
 
 		expect(() => {
 			const result = SessionManager.extractData('TOKEN');
@@ -68,18 +66,16 @@ describe('Decryption', function() {
 	});
 
 	it('With key length not equals to 32', async() => {
-		const SessionManager = require('./../index');
-		require('./../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
-		require('./../SessionManager').setKey(
-			'SECRET-RUNTHEONS-KEY-THAT-IS-NOT-32B'
-		);
+		const SessionManager = require('../index');
+		require('../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
+		require('../SessionManager').setKey('SECRET-RUNTHEONS-KEY-THAT-IS-NOT-32B');
 		expect(() => {
 			const result = SessionManager.extractData('');
 		}).toThrow();
 	});
 
 	it('With undefined token', async() => {
-		const SessionManager = require('./../index');
+		const SessionManager = require('../index');
 
 		const result = SessionManager.extractData(undefined);
 
@@ -87,18 +83,18 @@ describe('Decryption', function() {
 	});
 
 	it('With incorrect token', async() => {
-		const SessionManager = require('./../index');
+		const SessionManager = require('../index');
 
-		require('./../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
-		require('./../SessionManager').setKey('SECRET-RUNTHEONS-KEY-THAT-IS-32B');
+		require('../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
+		require('../SessionManager').setKey('SECRET-RUNTHEONS-KEY-THAT-IS-32B');
 		const result = SessionManager.extractData('TOKEN');
 		expect(result).toBe(undefined);
 	});
 
 	it('Example', async() => {
-		require('./../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
-		require('./../SessionManager').setKey('SECRET-RUNTHEONS-KEY-THAT-IS-32B');
-		const SessionManager = require('./../index');
+		require('../SessionManager').setJWTKey('SECRET-RUNTHEONS-JWT');
+		require('../SessionManager').setKey('SECRET-RUNTHEONS-KEY-THAT-IS-32B');
+		const SessionManager = require('../index');
 		const token = SessionManager.generateToken(data);
 		const result = SessionManager.extractData(token);
 
